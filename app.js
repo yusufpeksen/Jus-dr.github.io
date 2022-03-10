44405eaa24e0b14694eabd525b6a0e6e7bd90459
Ahmed Yusuf Pekşen <ahmetpeksen77@gmail.com>
2022-03-10 14:58:45 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const bar = document.querySelector(".fa-bars");
+const aside = document.querySelector("aside");
+const closeBtn = document.querySelector(".close-btn");
+const logo = document.querySelector(".logo");
+const buttons = document.querySelector(".buttons");
+
