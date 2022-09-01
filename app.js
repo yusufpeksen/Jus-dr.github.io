cc9fdbe3016aa58fb307786267b1391e3910e26e
Ahmed Yusuf Pekşen <68674132+Jus-dr@users.noreply.github.com>
2022-09-01 15:42:17 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const bar = document.querySelector(".fa-bars");
-const aside = document.querySelector("aside");
-const closeBtn = document.querySelector(".close-btn");
-const logo = document.querySelector(".logo");
-const buttons = document.querySelector(".buttons");
-
