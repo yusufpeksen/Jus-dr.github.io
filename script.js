3f9ced4fe54101072f70f015aed2539744353c99
Ahmed Yusuf Pekşen <68674132+Jus-dr@users.noreply.github.com>
2023-07-19 20:23:47 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let hiddenMenuBtn = document.querySelector("#hidden-menu-btn");
-let hiddenMenu = document.querySelector(".hidden-menu");
-
-
-hiddenMenuBtn.addEventListener("click" , function() {
-    hiddenMenu.classList.toggle("active");
-})
